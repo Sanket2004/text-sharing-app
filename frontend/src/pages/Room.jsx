4101a89115ef82cbcb2ce5564717789ec0312918
Sanket Banerjee <sanketbanerjee.2004@gmail.com>
2025-04-10 10:02:08 +0530

    room.js changed

--- frontend/src/pages/Room.jsx
+++ frontend/src/pages/Room.jsx
@@ -104,30 +104,32 @@ export default function Room() {
         {/* Messages */}
         <main
           ref={containerRef}
-          className="flex-1 p-4 overflow-y-auto relative"
+          className="flex-1 md:pt-10 p-4 overflow-y-auto relative"
         >
-          <div className="max-w-screen-lg space-y-4 mx-auto relative">
-            {messages.map((msg, i) => (
-              <div
-                key={i}
-                className="group border rounded-md bg-white px-4 py-3 shadow-sm text-sm relative"
-              >
-                <div className="text-gray-900 whitespace-pre-wrap break-words">
-                  {msg.message}
-                </div>
-                <div className="text-[11px] text-gray-400 mt-1">
-                  @{msg.username} •{" "}
-                  {new Date(msg.createdAt).toLocaleTimeString()}
-                </div>
-                <button
-                  onClick={() => handleCopy(msg.message)}
-                  className="absolute top-2 right-2 text-gray-400 hover:text-black opacity-0 group-hover:opacity-100 transition"
+          <div className="max-w-screen-lg mx-auto relative">
+            <div className="min-h-[70vh] space-y-4">
+              {messages.map((msg, i) => (
+                <div
+                  key={i}
+                  className="group border rounded-md bg-white px-4 py-3 shadow-sm text-sm relative"
                 >
-                  <Copy className="w-4 h-4" />
-                </button>
-              </div>
-            ))}
-            <div ref={endRef} />
+                  <div className="text-gray-900 whitespace-pre-wrap break-words">
+                    {msg.message}
+                  </div>
+                  <div className="text-[11px] text-gray-400 mt-1">
+                    @{msg.username} •{" "}
+                    {new Date(msg.createdAt).toLocaleTimeString()}
+                  </div>
+                  <button
+                    onClick={() => handleCopy(msg.message)}
+                    className="absolute top-2 right-2 text-gray-400 hover:text-black opacity-0 group-hover:opacity-100 transition"
+                  >
+                    <Copy className="w-4 h-4" />
+                  </button>
+                </div>
+              ))}
+              <div ref={endRef} />
+            </div>
 
             {/* Message Input */}
             <footer className="bg-white flex items-start sticky bottom-0 z-10 border-2 rounded-xl overflow-hidden w-full max-w-screen-lg mx-auto">
